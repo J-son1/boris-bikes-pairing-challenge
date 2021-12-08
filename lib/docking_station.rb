880c211582ec00fd0892f44e0045b7573efc4a42
icvotria <icvotria@gmail.com>
2021-12-08 21:01:40 +0000

    ustep 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,14 @@
 class DockingStation
+  def initialize
+    @bike = bike
+  end
+
+  attr_reader :bike
+
   def release_bike
+    Bike.new
+  end
+
+  def dock(bike)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,23 @@
 require 'docking_station'
 
 describe DockingStation do
+  docking_station = DockingStation.new
+
   it 'releases bike' do
-    docking_station = DockingStation.new
     expect(docking_station).to respond_to(:release_bike)
+  end
+
+  it 'gets a bike' do
+    bike = docking_station.release_bike
+    expect(bike).to respond_to(:working?)
+  end
+
+  it 'checks bike is working' do
+    bike = docking_station.release_bike
+    expect(bike.working?).to eq(true)
+  end
+
+  it 'docks a bike' do
+    is_expected.to respond_to(:dock).with(1).argument
   end
 end
